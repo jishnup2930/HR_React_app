827004da19c328a78cd8e2d60ccab21052d673c9
jishnu <jishnup15@gmail.com>
2023-12-19 02:29:40 +0530

    Update: Revised routing configuration in App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-
-import { useState } from 'react';
 import About from './components/about/index.js';
 import Employees from './components/employees/index.js';
 import NavBar from './components/Header/index.js'
 import Home from './components/home/index.js';
 import { Route, Routes } from 'react-router-dom';
+// import LoginForm from './components/signin/signin.js';
+import EmployeeDetails from './components/employeeDetails/employeeDetails.js';
+import LeaveForm from './components/Leave/leave.js';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
 
-      <Routes>
+           <NavBar />
+           <Routes>
+
+        {/* <Route path="/" element={<LoginForm />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/employees/" element={<Employees />} /> 
+        <Route path="/employees/:id" element={<EmployeeDetails />} />
+        <Route path="/employees/:id" element={<LeaveForm />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </div>
