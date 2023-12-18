52903b37e94ee21e0e5416b3fd08f1ea4057f25b
jishnu <jishnup15@gmail.com>
2023-12-19 02:32:58 +0530

    Refactor: Improved rendering structure and performed minor fixes

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <Router>
       
     <App />
-  
+
     </Router>
   </React.StrictMode>
 );
