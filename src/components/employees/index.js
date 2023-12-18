7d88e05abe971826cbf32c5d71bcd3062980ff87
jishnu <jishnup15@gmail.com>
2023-12-19 02:22:12 +0530

    Enhancement: Added scrollToTop and minor updates in Employee component

--- src/components/employees/index.js
+++ src/components/employees/index.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./employees.css";
+import { NavLink } from "react-router-dom";
 
 export default function Employee() {
   const [employee, setEmployee] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:5000/employees")
       .then((response) => response.json())
@@ -10,15 +12,21 @@ export default function Employee() {
         setEmployee(data);
       });
   }, []);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" 
+    });
+  };
   return (
     <div className="container-fluid">
       <div className="row">
         <h1>Employee Details</h1>
-        <div className="col-3">
+        <div className="col-6">
           <div className="list">
             {employee.map((data) => (
-              <p>
-                {data.fname} {data.lname}
+              <p key={'data.id'}>
+                <NavLink id="key" to={`/employees/${data.id}`} onClick={scrollToTop} >{data.id}. {data.fname} {data.lname} </NavLink>
               </p>
             ))}
           </div>
