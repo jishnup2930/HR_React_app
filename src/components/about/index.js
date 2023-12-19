98599f1c89eb91380435103c8b0b3f6babfd2111
jishnu <jishnup15@gmail.com>
2023-12-19 17:25:40 +0530

    style: Update formatting and structure in About component

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,72 +1,114 @@
-import React from 'react';
-import './about.css';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "./about.css";
+import { NavLink } from "react-router-dom";
 
 function About() {
-    return (
-        <div className="container-fluid">
-            <div>
-            <div>
-                <NavLink  to={"/employees"}>Back to employees</NavLink>
-            </div>
-                <h2>About Hamon</h2>
-                <p>
-                    <li>
-                    Hamon is a dynamic and innovative software development company that prides itself on pushing the boundaries of technology to create cutting-edge solutions. Established with a vision to revolutionize the digital landscape, Hamon focuses on delivering high-quality software products and services tailored to meet the diverse needs of its clients.
-                    </li>
-                </p>
+  return (
+    <div className="container-fluid">
+      <div>
+        <NavLink to={"/home"} className="btn btn-secondary btn-lg">
+          Back to Home
+        </NavLink>
+        
+        <h2>About Hamon</h2>
+        <p>
+          Hamon is a dynamic and innovative software development company that
+          prides itself on pushing the boundaries of technology to create
+          cutting-edge solutions. Established with a vision to revolutionize the
+          digital landscape, Hamon focuses on delivering high-quality software
+          products and services tailored to meet the diverse needs of its
+          clients.
+        </p>
+        <h3>Vision and Mission</h3>
+        <p>
+          <strong>Vision:</strong> To be a trailblazer in the software industry,
+          continually driving innovation and setting new benchmarks.
+        </p>
+        <strong>Mission:</strong>
+        <ul>
+          <ul>
+            <strong>Innovation:</strong> Constantly exploring new technologies
+            and methodologies to develop innovative software solutions.
+          </ul>
+          <ul>
+            <strong>Client-Centric Approach:</strong> Placing clients at the
+            core, ensuring tailored solutions that align with their business
+            goals and aspirations.
+          </ul>
+          <strong>Quality and Excellence:</strong> Commitment to delivering
+          top-notch, reliable, and scalable software solutions by maintaining
+          stringent quality standards
+        </ul>
+        <h3>Services Offered</h3>
 
-                <h3>Vision and Mission</h3>
-                <p>
-                    <li>
-                    <strong>Vision:</strong> To be a trailblazer in the software industry, continually driving innovation and setting new benchmarks.
-                    </li>
-                    </p>
-                <ul>
-                    <li>
-                <strong>Mission:</strong>
-                </li>
-
-                    <li><strong>Innovation:</strong> Constantly exploring new technologies and methodologies to develop innovative software solutions.</li>
-                    <li><strong>Client-Centric Approach:</strong> Placing clients at the core, ensuring tailored solutions that align with their business goals and aspirations.</li>
-                    <li><strong>Quality and Excellence:</strong> Commitment to delivering top-notch, reliable, and scalable software solutions by maintaining stringent quality standards.</li>
-                </ul>
-
-                <h3>Services Offered</h3>
-                <ul>
-                    <li><strong>Custom Software Development:</strong> Offering bespoke software solutions designed to address specific client needs, leveraging the latest technologies and frameworks.</li>
-                    <li><strong>Web and Mobile App Development:</strong> Crafting user-friendly, responsive, and feature-rich web and mobile applications to enhance user experiences across platforms.</li>
-                    <li><strong>AI and Machine Learning Solutions:</strong> Harnessing the power of artificial intelligence and machine learning to develop intelligent software systems for automation and data-driven decision-making.</li>
-                    <li><strong>Consulting and IT Services:</strong> Providing comprehensive consulting services, including IT strategy, system integration, and IT infrastructure support.</li>
-                </ul>
-
-                <h3>Company Culture</h3>
-                <p>
-                    <li>Hamon fosters a culture of innovation, collaboration, and continuous learning. Employees are encouraged to explore new ideas, contribute creatively, and engage in professional development initiatives. The company prioritizes a healthy work-life balance, acknowledging the significance of employee well-being in driving productivity and innovation.
-               
-                    </li>
-                     </p>
-
-                <h3>Core Values</h3>
-                <ul>
-                    <li><strong>Innovation:</strong> Encouraging a culture of creativity, exploration, and forward-thinking to stay ahead in a rapidly evolving industry.</li>
-                    <li><strong>Client Satisfaction:</strong> Prioritizing client needs and ensuring their satisfaction by delivering high-quality, tailored solutions.</li>
-                    <li><strong>Integrity:</strong> Upholding ethical standards and transparency in all business dealings, maintaining trust and credibility with clients and partners.</li>
-                    <li><strong>Team Collaboration:</strong> Promoting a collaborative work environment where diverse talents come together to achieve common goals.</li>
-                </ul>
-
-                <h3>Impact and Achievements</h3>
-                <p>
-                    Hamon's commitment to excellence has resulted in the successful development and implementation of transformative software solutions for various industries. Their impactful contributions have streamlined processes, increased efficiency, and driven growth for their clients, establishing a reputation for reliability and innovation within the tech community.
-                </p>
-
-                <p>
-                    Overall, Hamon stands as a forward-thinking software development company dedicated to driving digital transformation and empowering businesses through technology.
-                </p>
-            </div>
-
-        </div>
-    );
+        <ul>
+          <strong>Custom Software Development:</strong> Offering bespoke
+          software solutions designed to address specific client needs,
+          leveraging the latest technologies and frameworks.
+        </ul>
+        <ul>
+          <strong>Web and Mobile App Development:</strong> Crafting
+          user-friendly, responsive, and feature-rich web and mobile
+          applications to enhance user experiences across platforms.
+        </ul>
+        <ul>
+          <strong>AI and Machine Learning Solutions:</strong> Harnessing the
+          power of artificial intelligence and machine learning to develop
+          intelligent software systems for automation and data-driven
+          decision-making.
+        </ul>
+        <ul>
+          <strong>Consulting and IT Services:</strong> Providing comprehensive
+          consulting services, including IT strategy, system integration, and IT
+          infrastructure support.
+        </ul>
+        <h3>Company Culture</h3>
+        <p>
+          Hamon fosters a culture of innovation, collaboration, and continuous
+          learning. Employees are encouraged to explore new ideas, contribute
+          creatively, and engage in professional development initiatives. The
+          company prioritizes a healthy work-life balance, acknowledging the
+          significance of employee well-being in driving productivity and
+          innovation.
+        </p>
+        <h3>Core Values</h3>
+        <ul>
+          <strong>Innovation:</strong> Encouraging a culture of creativity,
+          exploration, and forward-thinking to stay ahead in a rapidly evolving
+          industry.
+        </ul>
+        <ul>
+          <strong>Client Satisfaction:</strong> Prioritizing client needs and
+          ensuring their satisfaction by delivering high-quality, tailored
+          solutions.
+        </ul>
+        <ul>
+          <strong>Integrity:</strong> Upholding ethical standards and
+          transparency in all business dealings, maintaining trust and
+          credibility with clients and partners.
+        </ul>
+        <ul>
+          <strong>Team Collaboration:</strong> Promoting a collaborative work
+          environment where diverse talents come together to achieve common
+          goals.
+        </ul>
+        <h3>Impact and Achievements</h3>
+        <p>
+          Hamon's commitment to excellence has resulted in the successful
+          development and implementation of transformative software solutions
+          for various industries. Their impactful contributions have streamlined
+          processes, increased efficiency, and driven growth for their clients,
+          establishing a reputation for reliability and innovation within the
+          tech community.
+        </p>
+        <p>
+          Overall, Hamon stands as a forward-thinking software development
+          company dedicated to driving digital transformation and empowering
+          businesses through technology.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
