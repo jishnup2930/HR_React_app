5f690603612063516678affa23d4c4278c62a3fd
jishnu <jishnup15@gmail.com>
2023-12-19 17:10:08 +0530

    feat: Add Contact component with email, phone, and address details

---
+++ src/components/contact/contact.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Contact = () => {
+  const emailAddress = 'contact@hamontech.com';
+  const phoneNumber = '+1234567890';
+  const address = 'Hamon Building, Near Kuthiravattam, Kozhikode';
+
+
+  return (
+    <div className="contact-container">
+      <h1>Hamon Technologies</h1>
+      <div id='address'>
+        <h3>Address</h3>
+        <p>{address}</p>
+      </div>
+      <p>
+        For inquiries, please contact us via:
+        <div id='contact'>
+          <NavLink>
+            {emailAddress}
+          </NavLink>
+          <br />
+          <NavLink>
+            {phoneNumber}
+          </NavLink>
+        </div>
+      </p>
+      <div>
+        <NavLink to={"/home"} className="btn btn-secondary btn-lg">
+          Back to Home
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
