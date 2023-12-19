8bbdfffbeb09ba1f03ba9f85eb9d15f6c7670573
jishnu <jishnup15@gmail.com>
2023-12-19 17:39:54 +0530

    feat: Update Home component with additional content and a contact link

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,19 +1,31 @@
-import React,{Fragment} from "react"
-import './home.css'
-function Home (){
-return (
-    <Fragment>
-    <body>
-    <div>
-        <h1>WELCOME TO HAMON HRMS</h1>
-    </div>
-    <div>
-    <form action="/signup" method="post" id="signup"> 
-    </form>
-    </div>
-    </body>
+import React, { Fragment } from "react";
+import "./home.css";
+import { NavLink } from "react-router-dom";
 
+function Home() {
+  return (
+    <Fragment>
+      <body>
+        <div className="home">
+          <div>
+            <h1>WELCOME TO HAMON HRMS</h1>
+          </div>
+          <div>
+            <p>
+              Hamon Technologies specializes in harnessing cutting-edge software
+              solutions to drive your business forward or bring your innovative
+              ideas to life.
+            </p>
+          </div>
+          <div>
+            <NavLink to={"/contact"} className="btn btn-secondary btn-lg">
+              Get in touch
+            </NavLink>
+          </div>
+        </div>
+      </body>
     </Fragment>
-);
+  );
 }
-export default Home
+
+export default Home;
