39d97b5b93b67d34d531961ff60962a77d73f707
jishnu <jishnup15@gmail.com>
2023-12-19 17:13:33 +0530

    feat: Update routing in App component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Employees from './components/employees/index.js';
 import NavBar from './components/Header/index.js'
 import Home from './components/home/index.js';
 import { Route, Routes } from 'react-router-dom';
-// import LoginForm from './components/signin/signin.js';
+import LoginForm from './components/signin/signin.js';
 import EmployeeDetails from './components/employeeDetails/employeeDetails.js';
 import LeaveForm from './components/Leave/leave.js';
+import Contact from './components/contact/contact.js';
 
 function App() {
   return (
@@ -14,13 +15,13 @@ function App() {
            <NavBar />
            <Routes>
 
-        {/* <Route path="/" element={<LoginForm />} /> */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginForm />} />
         <Route path="/home" element={<Home />} />
         <Route path="/employees/" element={<Employees />} /> 
         <Route path="/employees/:id" element={<EmployeeDetails />} />
         <Route path="/employees/:id" element={<LeaveForm />} />
         <Route path="/about" element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   );
