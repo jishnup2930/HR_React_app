48683a07eccc55b56769f677583a09bed1b53fbe
jishnu <jishnup15@gmail.com>
2023-12-20 19:38:43 +0530

    Refactored routing logic to conditionally render NavBar based on location

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import About from './components/about/index.js';
-import Employees from './components/employees/index.js';
-import NavBar from './components/Header/index.js'
-import Home from './components/home/index.js';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import NavBar from './components/Header/index.js';
 import LoginForm from './components/signin/signin.js';
+import Home from './components/home/index.js';
+import Employees from './components/employees/index.js';
 import EmployeeDetails from './components/employeeDetails/employeeDetails.js';
 import LeaveForm from './components/Leave/leave.js';
+import About from './components/about/index.js';
 import Contact from './components/contact/contact.js';
 
 function App() {
+  const location = useLocation();
+  const showNavBar = location.pathname !== '/'; 
+
   return (
     <div className="App">
-
-           <NavBar />
-           <Routes>
-
+      {showNavBar && <NavBar />} 
+      <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/employees/" element={<Employees />} /> 
+        <Route path="/employees/" element={<Employees />} />
         <Route path="/employees/:id" element={<EmployeeDetails />} />
         <Route path="/employees/:id" element={<LeaveForm />} />
         <Route path="/about" element={<About />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
